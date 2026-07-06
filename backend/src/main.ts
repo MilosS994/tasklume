8656af3b507ae9bc74103feec74b8c9b59a916d4
Milos Srejic <srejic.milos94@gmail.com>
2026-07-06 19:48:32 +0200

    chore: setup validation pipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+const IS_PRODUCTION = (process.env.NODE_ENV as string) === 'production';
+
+const pipeOptions = {
+  whitelist: true,
+  forbidNonWhitelisted: true,
+  transform: true,
+  disableErrorMessages: IS_PRODUCTION,
+};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe(pipeOptions));
   await app.listen(process.env.PORT ?? 3000);
 }
 
